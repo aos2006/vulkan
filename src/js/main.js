306f6db66ea1cde0ccdde350972ac30b0fca690d
anton <vashchenko-01@mail.ru>
2016-10-12 12:32:45 +0300

    init

---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+  var inputTel = $('.js-phone');
+  var item = $('.js-item');
+  var modal = $('.js-modal');
+  var buttonSubmit = $('.js-submit');
+
+
+  buttonSubmit.on('click', function(ev){
+    ev.preventDefault();
+    var selfInput = $(ev.currentTarget).parent().find('.js-phone');
+    var isChcked = $($(ev.currentTarget).parent().find('.js-checkbox')).is(':checked');
+    var isValidInput = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(selfInput.val());
+    if (isChcked && isValidInput) {
+      selfInput.removeClass('form__input--error');
+      console.log('go');
+      $.ajax({
+        url: '',
+        method: 'post',
+        data: {
+          phone: selfInput.val()
+        }
+      })
+      return;
+    }
+
+    if (!isValidInput && selfInput.val() === '') {
+      console.log('not valid');
+      selfInput.addClass('form__input--error');
+    }
+
+    if (!isChcked) {
+      console.log('not checked');
+      return;
+    }
+  })
+
+  item.on('click', function() {
+    modal.addClass('popup--show');
+  })
+
+  modal.on('click', function(ev) {
+    if ($(ev.target).hasClass('js-modal')) {
+      modal.removeClass('popup--show');
+    }
+
+    return;
+  })
+
+  inputTel.inputmask({
+    alias: 'phone',
+    allowDropdown: false
+  })
+})
